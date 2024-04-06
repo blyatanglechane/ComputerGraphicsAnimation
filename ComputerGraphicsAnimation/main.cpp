1ee9183c999e24c34517415785929c9063406cbf
Admin <Kazimirov4567@mail.ru>
2024-04-06 20:53:04 +0300

    1-ая часть второй лабы готова. Полностью рабочая.

--- ComputerGraphicsAnimation/main.cpp
+++ ComputerGraphicsAnimation/main.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
-float x, y;
+float x, y; // Для отрисовки сетки
+float centerX, centerY; // Для координат центра фигуры
 float rotationAngle = 0.0;
 float scale = 1.0f;
+float initialCenterX, initialCenterY;
 
 void reflectXEqualsY(float& x, float& y)
 {
@@ -16,63 +18,84 @@ void reflectXEqualsY(float& x, float& y)
 
 void DrawShape()
 {
-    // Размеры фигуры
+    /* Размеры фигуры*/
     float size = 0.1f / scale;
 
-    // Рисуем фигуру с учетом масштаба и поворота
+    /* Рисуем фигуру с учетом масштаба и поворота*/
     glPushMatrix();
-    glTranslatef(x, y, 0.0f); // Перемещение в указанную точку
+    glTranslatef(centerX, centerY, 0.0f); // Перемещение в указанную точку
     glRotatef(rotationAngle, 0.0f, 0.0f, 1.0f); // Поворот относительно оси Z
 
-    // Рисуем ромб
+    /* Рисуем ромб*/
     glBegin(GL_LINES);
     glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex2f(x - 0.25 - size, y);
-    glVertex2f(x, y + 0.25 + size);
+    glVertex2f(centerX - 0.25 - size, centerY);
+    glVertex2f(centerX, centerY + 0.25 + size);
 
-    glVertex2f(x, y + 0.25 + size);
-    glVertex2f(x + 0.25 + size, y);
+    glVertex2f(centerX, centerY + 0.25 + size);
+    glVertex2f(centerX + 0.25 + size, centerY);
 
-    glVertex2f(x + 0.25 + size, y);
-    glVertex2f(x, y - 0.25 - size);
+    glVertex2f(centerX + 0.25 + size, centerY);
+    glVertex2f(centerX, centerY - 0.25 - size);
 
-    glVertex2f(x, y - 0.25 - size);
-    glVertex2f(x - 0.25 - size, y);
+    glVertex2f(centerX, centerY - 0.25 - size);
+    glVertex2f(centerX - 0.25 - size, centerY);
 
-    // Отрсиовка крестовины
-    glVertex2f(x, y + 0.25 + size);
-    glVertex2f(x, y - 0.25 - size);
+    /* Отрисовка крестовины*/
+    glVertex2f(centerX, centerY + 0.25 + size);
+    glVertex2f(centerX, centerY - 0.25 - size);
 
-    glVertex2f(x + 0.25 + size, y);
-    glVertex2f(x - 0.25 - size, y);
+    glVertex2f(centerX + 0.25 + size, centerY);
+    glVertex2f(centerX - 0.25 - size, centerY);
 
-    // Отрисовка звезды
-    glVertex2f(x - 0.25 - size, y);
-    glVertex2f(x - 0.05 - size * 0.25, y + 0.05 + size * 0.25);
+    /*  Отрисовка звезды*/
+    glVertex2f(centerX - 0.25 - size, centerY);
+    glVertex2f(centerX - 0.05 - size * 0.25, centerY + 0.05 + size * 0.25);
 
-    glVertex2f(x - 0.05 - size * 0.25, y + 0.05 + size * 0.25);
-    glVertex2f(x, y + 0.25 + size);
+    glVertex2f(centerX - 0.05 - size * 0.25, centerY + 0.05 + size * 0.25);
+    glVertex2f(centerX, centerY + 0.25 + size);
 
-    glVertex2f(x, y + 0.25 + size);
-    glVertex2f(x + 0.05 + size * 0.25, y + 0.05 + size * 0.25);
+    glVertex2f(centerX, centerY + 0.25 + size);
+    glVertex2f(centerX + 0.05 + size * 0.25, centerY + 0.05 + size * 0.25);
 
-    glVertex2f(x + 0.05 + size * 0.25, y + 0.05 + size * 0.25);
-    glVertex2f(x + 0.25 + size, y);
+    glVertex2f(centerX + 0.05 + size * 0.25, centerY + 0.05 + size * 0.25);
+    glVertex2f(centerX + 0.25 + size, centerY);
 
-    glVertex2f(x + 0.25 + size, y);
-    glVertex2f(x + 0.05 + size * 0.25, y - 0.05 - size * 0.25);
+    glVertex2f(centerX + 0.25 + size, centerY);
+    glVertex2f(centerX + 0.05 + size * 0.25, centerY - 0.05 - size * 0.25);
 
-    glVertex2f(x + 0.05 + size * 0.25, y - 0.05 - size * 0.25);
-    glVertex2f(x, y - 0.25 - size);
+    glVertex2f(centerX + 0.05 + size * 0.25, centerY - 0.05 - size * 0.25);
+    glVertex2f(centerX, centerY - 0.25 - size);
 
-    glVertex2f(x, y - 0.25 - size);
-    glVertex2f(x - 0.05 - size * 0.25, y - 0.05 - size * 0.25);
+    glVertex2f(centerX, centerY - 0.25 - size);
+    glVertex2f(centerX - 0.05 - size * 0.25, centerY - 0.05 - size * 0.25);
 
-    glVertex2f(x - 0.05 - size * 0.25, y - 0.05 - size * 0.25);
-    glVertex2f(x - 0.25 - size, y);
+    glVertex2f(centerX - 0.05 - size * 0.25, centerY - 0.05 - size * 0.25);
+    glVertex2f(centerX - 0.25 - size, centerY);
     glEnd();
-    glPopMatrix;
+    glPopMatrix();
 }
+
+void resetShape()
+{
+    centerX = 0;
+    centerY = 0;
+    x = initialCenterX;
+    y = initialCenterY;
+    rotationAngle = 0.0f;
+    scale = 1.0f;
+    DrawShape();
+}
+
+void rotateAroundPoint(float angle, float pointX, float pointY)
+{
+    glPushMatrix();
+    glTranslatef(pointX, pointY, 0.0f); // Смещаем систему координат в точку (pointX, pointY)
+    glRotatef(angle, 0.0f, 0.0f, 1.0f); // Поворачиваем систему координат
+    glTranslatef(-pointX, -pointY, 0.0f); // Возвращаем систему координат в исходное положение
+    glPopMatrix();
+}
+
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
@@ -106,11 +129,11 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     }
     else if (key == GLFW_KEY_U && action == GLFW_PRESS)
     {
-        rotationAngle += 5.0f; // Поворот против часовой стрелки на 5 градусов
+        rotationAngle += 45.0f; // Поворот против часовой стрелки на 5 градусов
     }
     else if (key == GLFW_KEY_I && action == GLFW_PRESS)
     {
-        rotationAngle -= 5.0f; // Поворот по часовой стрелке на 5 градусов
+        rotationAngle -= 45.0f; // Поворот по часовой стрелке на 5 градусов
     }
     // Отражение относительно оси OX
     else if (key == GLFW_KEY_Z && action == GLFW_PRESS)
@@ -126,6 +149,24 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     else if (key == GLFW_KEY_C && action == GLFW_PRESS)
     {
         reflectXEqualsY(x, y);
+    }
+    // Поворот на заданный угол относительно произвольной точки, указываемой в ходе выполнения программы.
+    else if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+    {
+        // Запрашиваем у пользователя угол и координаты точки
+        cout << "Введите угол поворота: ";
+        cin >> rotationAngle;
+        cout << "Введите координаты точки поворота (x y): ";
+        cin >> centerX >> centerY;
+
+        // Поворачиваем фигуру относительно заданной точки
+        rotateAroundPoint(rotationAngle, centerX, centerY);
+    }
+    // Вернуть фигуру в исходное положение
+    else if (key == GLFW_KEY_TAB && action == GLFW_PRESS)
+    {
+        // Возврат фигуры в исходное положение
+        resetShape();
     }
 }
 
@@ -191,6 +232,8 @@ int main(void)
     cout << "Введите координаты центра фигуры\n";
     cin >> x >> y;
 
+    initialCenterX = x;
+    initialCenterY = y;
     /* Цикл до закрытия окна */
     while (!glfwWindowShouldClose(window))
     {
